222e39bb2f78d3ddcea4e343bee5d750d143847f
Zorro <ZorroSkii@outlook.com>
2020-03-09 19:35:15 +0800

    Test Commit

--- src/cn/zorro/git/GoodTest.java
+++ src/cn/zorro/git/GoodTest.java
@@ -1,5 +1,15 @@
 package cn.zorro.git;
 
-public class GoodTest {
+/**
+ * Test Commit
+ * 可以不用add，直接Commit
+ * 可以直接用crtl+shift+3，也就是crtl+# 快捷commit
+ * @author Zorro
+ *
+ */
 
+public class GoodTest {
+	public static void main(String[] args) {
+		System.out.println("Test commit");
+	}
 }
